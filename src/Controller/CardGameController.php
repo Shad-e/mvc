ff645f438eb03125766fd51eadc945ff1abaf2a3
Shad-e <lisd22@student.bth.se>
2024-04-17 21:24:09 +0200

    Controller for the routes for the card game

---
+++ src/Controller/CardGameController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CardGameController extends AbstractController
+{
+    #[Route("/game/card", name: "card_start")]
+    public function home(): Response
+    {
+        return $this->render('card/home.html.twig');
+    }
+}
