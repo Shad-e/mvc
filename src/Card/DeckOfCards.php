1e4e7b6753791b7386df11416744fc91a9e26767
Shad-e <lisd22@student.bth.se>
2024-04-23 16:41:33 +0200

    Added compability with joker class.

--- src/Card/DeckOfCards.php
+++ src/Card/DeckOfCards.php
@@ -6,15 +6,21 @@ class DeckOfCards
 {
     protected $cards = [];
 
-    public function __construct()
+    public function __construct($includeJokers = true)
     {
-        $this->initializeDeck();
+        $this->initializeDeck($includeJokers);
     }
 
-    protected function initializeDeck()
+    protected function initializeDeck($includeJokers)
     {
         foreach (Card::CARDS as $name => $symbol) {
             $this->cards[] = new Card($name);
+        }
+
+        if ($includeJokers) {
+            foreach (JokerCard::JOKER_CARDS as $name => $symbol) {
+                $this->cards[] = new JokerCard($name);
+            }
         }
     }
 
